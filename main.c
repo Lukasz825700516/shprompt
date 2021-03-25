d5b6bae886b554454b4a55758117ef29ee4fb56b
Łukasz <lukasz@lukaszm.xyz>
2021-03-25 20:47:24 +0100

    Fixed #1

--- main.c
+++ main.c
@@ -56,41 +56,44 @@ shprompt_get_directory(const char* path) {
 	return span;
 }
 
-int git_repo_prompt(git_buf* repo_root) {
+int git_repo_prompt(const char* directory) {
 	char padding[256];
 	memset(padding, ' ', 256);
 
-	struct str_span repo_name = shprompt_get_repo(repo_root->ptr);
-	struct winsize dimensions;
-
-	ioctl(0, TIOCGWINSZ, &dimensions);
-
-	git_repository *repo;
-	if (git_repository_open(&repo, "./")) {
-		// Current directory is not in any git repo
-
-		return EXIT_FAILURE;
-	}
-
-	git_reference *ref;
-	if (git_repository_head(&ref, repo)) {
-		// HEAD is not set? Create initial commit!
-
-		return EXIT_FAILURE;
+	git_buf repo_root = {0};
+	if (git_repository_discover(&repo_root, directory, 0, NULL)) {
 
 	} else {
-		const char* branch_name;
-		git_branch_name(&branch_name, ref);
+		struct str_span repo_name = shprompt_get_repo(repo_root.ptr);
+		struct winsize dimensions;
 
-		size_t branch_name_width = strlen(branch_name);
-
-		int repo_name_width = (repo_name.str_end - repo_name.str);
-		printf("%.*s%.*s%s\n", repo_name_width, repo_name.str, dimensions.ws_col - repo_name_width - (int)branch_name_width, padding, branch_name);
+		ioctl(0, TIOCGWINSZ, &dimensions);
 
 
-		git_reference_free(ref);
+		git_repository *repo;
+		if (git_repository_open(&repo,repo_root.ptr)) {
+			// Current directory is not in any git repo
+
+		}
+
+		git_reference *ref;
+		if (git_repository_head(&ref, repo)) {
+			// HEAD is not set? Create initial commit!
+
+		} else {
+			const char* branch_name;
+			git_branch_name(&branch_name, ref);
+
+			size_t branch_name_width = strlen(branch_name);
+
+			int repo_name_width = (repo_name.str_end - repo_name.str);
+			printf("%.*s%.*s%s\n", repo_name_width, repo_name.str, dimensions.ws_col - repo_name_width - (int)branch_name_width, padding, branch_name);
+
+
+			git_reference_free(ref);
+		}
+		git_buf_free(&repo_root);
 	}
-
 	return 0;
 }
 
@@ -100,29 +103,16 @@ int main() {
 
 	const char* user = getenv("USER");
 	char* directory = getcwd(0, 0);
-	git_buf repo_root = {0};
 	char host[HOST_NAME_MAX + 1];
 	struct str_span directory_span = shprompt_get_directory(directory);
-
-
 
 	gethostname(host, HOST_NAME_MAX + 1);
 
 	git_libgit2_init();
 
-	int error = git_repository_discover(&repo_root, directory, 0, NULL);
-
-
-	// print GIT's part of prompt
-	const int in_git_repo = repo_root.size > 0;
-	if (in_git_repo) {
-		git_repo_prompt(&repo_root);
-	}
-
-
+	git_repo_prompt(directory);
 	printf("%s@%s %.*s $ ", user, host, (int)(directory_span.str_end - directory_span.str), directory_span.str);
 
-	git_buf_free(&repo_root);
 	git_libgit2_shutdown();
 
 	free(directory);
